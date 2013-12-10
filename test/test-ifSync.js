206ece52781d1067c17c823c371bcceeeff6ab24
鬼道 <xukai.xk@taobao.com>
2013-12-10 20:27:44 +0800

    test-ifSync 更新参数

--- test/test-ifSync.js
+++ test/test-ifSync.js
@@ -1,12 +1,13 @@
 var ift = require('../lib/ift');
 var path = require('path');
 
-ift.ifInit({});
+ift.ifInit({root: __dirname});
 
 
 ift.ifSync({
-    files:[path.resolve("./demo/data/demo")],
-    savePath:path.resolve("./doc/demo.md"),
-    //dataTpl:path.resolve("./tpl/data.tpl"),
-    //dataIncTpl:path.resolve("./tpl/data.inc.tpl"),
+    dataDir: path.join(__dirname, 'demo/data'),
+    files: ['demo'],
+    savePath: path.resolve("./doc/demo.md"),
+    dataTpl: path.resolve("./tpl/data.tpl"),
+    dataIncTpl: path.resolve("./tpl/data.inc.tpl")
 })

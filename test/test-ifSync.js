3deddc4eb2eadb694f80aca7b1b4dc612aa302ea
鬼道 <xukai.xk@taobao.com>
2013-12-10 22:30:31 +0800

    add qunit

--- test/test-ifSync.js
+++ test/test-ifSync.js
@@ -1,13 +1,32 @@
-var ift = require('../lib/ift');
 var path = require('path');
+var fs = require('fs');
 
-ift.ifInit({root: __dirname});
+// 使用 root 是考虑到 `npm test` 在项目根目录下运行测试
 
+var root = __dirname;
+
+ift.ifInit({root: root});
 
 ift.ifSync({
-    dataDir: path.join(__dirname, 'demo/data'),
-    files: ['demo'],
-    savePath: path.resolve("./doc/demo.md"),
-    dataTpl: path.resolve("./tpl/data.tpl"),
-    dataIncTpl: path.resolve("./tpl/data.inc.tpl")
-})
+    dataDir: path.resolve(root, 'demo/data'),
+    files: ['demo.js'],
+    savePath: path.resolve(root, './doc/demo.md'),
+    dataTpl: path.resolve(root, './tpl/data.tpl'),
+    dataIncTpl: path.resolve(root, './tpl/data.inc.tpl')
+});
+
+QUnit.module('ift.ifInit');
+
+test('basic', function() {
+    ok(fs.existsSync(path.resolve(root, 'demo')));
+    ok(fs.existsSync(path.resolve(root, 'demo/data')));
+    ok(fs.existsSync(path.resolve(root, 'doc')));
+    ok(fs.existsSync(path.resolve(root, 'demo/data/if-config.json')));
+    ok(fs.existsSync(path.resolve(root, 'demo/data/demo.js')));
+});
+
+QUnit.module('ift.ifSync');
+
+test('basic', function() {
+    ok(fs.existsSync(path.resolve(root, 'doc/demo.md')));
+});
